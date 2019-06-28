0b7f47a56d4769a6ad404442686e28f1a97b02b7
Benjamin Ethington <benaminde@gmail.com>
2019-06-27 19:49:00 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mv ./data ../data
-docker build --build-arg VERSION=v0.17.1 -t bethington/litecoind .
-docker build --build-arg VERSION=v0.17.1 -t bethington/litecoind:v0.17.1 .
+docker build --build-arg VERSION=v0.17.1 --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 -t bethington/litecoind .
+docker build --build-arg VERSION=v0.17.1 --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 -t bethington/litecoind:v0.17.1 .
 mv ../data ./data
